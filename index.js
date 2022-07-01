1fa5e6fd5c57e74a858ac14a86b5cc2640d172cd
Your Name <you@example.com>
2022-07-01 19:45:52 +0100

    commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const num2 = parseFloat(prompt('Enter number2'));
 const operator = prompt('Enter operator(+, -, /, *)');
 let result = 0;
 if (isNaN(num1) || isNaN(num2)) {
-    alert('Wrong Input Refresh the page again and privide data! ');
+    alert('Wrong Input Refresh the page again and provide data! ');
 } else {
 
     if (operator == '+') {
